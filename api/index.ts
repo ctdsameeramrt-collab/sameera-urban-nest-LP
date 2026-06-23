41e8e2a07dac997bdb8217f8cb003ad80d3fcff9
ctdsameeramrt-collab <ctdsameeramrt@gmail.com>
2026-06-23 18:56:45 +0530

    Delete api/index.ts

--- api/index.ts
+++
@@ -1,7 +0,0 @@
-import path from 'path';
-import fs from 'fs';
-
-// Dynamically import the server compiled by your build script
-const serverApp = require('../dist/server.cjs');
-
-export default serverApp;
